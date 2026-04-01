65d7e6241167ac1279f4fdd48516162edf088e64
Stuart Napier <stuart.napier@gov.scot>
2026-04-01 13:54:21 +0100

    remove extra code from end of function used in interactive development

--- R/scjs_load_ukds.R
+++ R/scjs_load_ukds.R
@@ -97,22 +97,3 @@ read_sav_data <- function(filepath, columns, dataset_type, year) {
   }
   return(data)
 }
-
-
-
-scjs_dfs <- scjs_load_ukds(Sys.getenv("USER_DATA_PATH"), years_to_load = 2019:2024)
-
-my_path <- Sys.getenv("USER_DATA_PATH")
-
-folder <- ukds_lookup[[1,3]]
-cur_path <- paste0(my_path, folder)
-
-found_path <- paste0(
-  cur_path,
-  "/",
-  list.files(cur_path, pattern = ukds_lookup[[1,4]], recursive = TRUE)
-)
-
-new_df <- haven::read_sav(found_path)
-
-purrr::imap(dplyr::pull(ukds_lookup, var="nvf_file"), ~paste(.x, .y))
