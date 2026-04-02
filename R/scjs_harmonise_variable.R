418553d128fb4cdc43572dfeadf9d7b34b5dd89f
Stuart Napier <stuart.napier@gov.scot>
2026-04-02 11:34:39 +0100

    add blank skeleton for scjs_harmonise_variable(), small update to documentation of scjs_load_ukds()

---
+++ R/scjs_harmonise_variable.R
@@ -0,0 +1,14 @@
+#' Harmonise variables from SCJS datasets
+#'
+#' @description
+#' Harmonises variables from individual SCJS datasets into a target single multi-year dataset.
+#'
+#'
+#' @param target_df The dataset that you want to harmonised variables to be added to
+#'
+#' @export
+scjs_harmonise_variable <- function(target_df) {
+
+}
+
+# scjs_data <- list(nvf_2023=df_test_2023, nvf_2024=df_test_2024)
