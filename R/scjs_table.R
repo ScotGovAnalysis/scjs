f04b7050abbc1ba45dda6656e7797bff4c06f3aa
Stuart Napier <stuart.napier@gov.scot>
2026-05-05 14:41:55 +0100

    added files to begin development of table function

---
+++ R/scjs_table.R
@@ -0,0 +1,10 @@
+#' Create a table from SCJS data
+#'
+#' @description
+#' Takes an individual or harmonised set of SCJS data and builds a table using standard methods.
+#'
+#' @export
+#'
+scjs_table <- function() {
+  print("test")
+}
