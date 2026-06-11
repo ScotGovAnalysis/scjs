e5e87e4061691fa9091cd9dc3f4b63038a35cb9d
Stuart Napier <stuart.napier@gov.scot>
2026-06-11 15:27:32 +0100

    add aggregate_variable function skeleton

---
+++ R/aggregate_variable.R
@@ -0,0 +1,10 @@
+#' Aggregate an SCJS Variable
+#'
+#' @param variable
+#'
+#' @return A modified column in a data frame.
+#' @export
+#'
+aggregate_variable <- function(variable) {
+  return("abc")
+}
